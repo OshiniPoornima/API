e800d6f1106a7f901bf17d01ef43f21de393a6f1
OshiniPoornima <97431939+OshiniPoornima@users.noreply.github.com>
2024-08-13 12:31:43 +0530

    Minor changes

--- server.js
+++ server.js
@@ -1,22 +1,83 @@
 const express= require('express')
 const mongoose= require('mongoose')
+const Product=require('./models/productModel')
 const app= express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
-//route
+//routes
 
 app.get('/',(req,res)=>{
     res.send('Hello NODE API')
 })
 
 app.get('/blog',(req,res)=>{
-    res.send('Hello Blog,          my name is Oshini')
+    res.send('Hello Blog, My name is Oshini')
+})
+
+app.get('/products',async(req,res)=>{
+    try {
+        const products= await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+})
+
+app.get('/products/:id', async(req,res)=>{
+    try {
+        const {id}=req.params;
+        const product= await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
 })
 
 
-app.post('/product',(req, res)=>{
-    console.log(req.body);
-    res.send(req.body)
+app.post('/products',async(req, res)=>{
+   try {
+        const product= await Product.create(req.body)
+        res.status(200).json(product);
+
+   } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message:error.message})
+   }
+})
+
+//update a product
+app.put('/products/:id', async(req,res)=>{
+    try {
+        const {id}=req.params;
+        const product=await Product.findByIdAndUpdate(id, req.body);
+       //we cannot find any product in database
+        if(!product){
+            return res.status(404).json({message:'cannot find any product with ID ${id}'})
+        }
+        const updatedProduct= await Product.findById(id);
+        res.status(200).json(updatedProduct);
+
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+})
+
+//delete a product
+
+app.delete('/products/:id', async(req,res)=>{
+    try {
+        const{id}=req.params;
+        const product= await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message:'cannot find any product with ID ${id}'})
+    } 
+    res.status(200).json(product);
+    
+ } catch (error) {
+        res.status(500).json({message:error.message})
+    }
 })
 
 mongoose.set("strictQuery",false)
