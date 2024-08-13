c9e615629c708a35748edcb7bb98c9d0cfaefa5a
OshiniPoornima <97431939+OshiniPoornima@users.noreply.github.com>
2024-08-13 12:29:58 +0530

    project upload

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express= require('express')
+const mongoose= require('mongoose')
 const app= express()
 
 
@@ -8,6 +9,24 @@ app.get('/',(req,res)=>{
     res.send('Hello NODE API')
 })
 
-app.listen(3000,()=>{
-    console.log('Node API app is running on port 3000')
+app.get('/blog',(req,res)=>{
+    res.send('Hello Blog,          my name is Oshini')
 })
+
+
+app.post('/product',(req, res)=>{
+    console.log(req.body);
+    res.send(req.body)
+})
+
+mongoose.set("strictQuery",false)
+mongoose.connect('mongodb://localhost:27017/')
+.then(()=>{
+    console.log('connected to MongoDB')
+    app.listen(3000,()=>{
+        console.log('Node API app is running on port 3000')
+    });
+}).catch((error)=>{
+    console.log(error)
+})
+
